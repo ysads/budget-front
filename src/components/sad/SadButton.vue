3151120f47d17f7ed1500aa7ff8d408e0aec7bea
Ygor Sad <ysads2010@gmail.com>
2020-09-18 19:24:53 -0300

    feat(sad/*): create SadButton and SadLabel

---
+++ src/components/sad/SadButton.vue
@@ -0,0 +1,97 @@
+<template>
+  <button
+    class="button"
+    :class="[typeClass, sizeClass]"
+    :size="size"
+    :type="type"
+    data-test="button"
+    @click="$emit('click')"
+  >
+    <i
+      v-if="icon"
+      class="button__icon icon fas"
+      :class="iconClass"
+      data-test="icon"
+    />
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      default: 'normal',
+      validator (val) {
+        return ['normal', 'small'].includes(val)
+      },
+    },
+
+    type: {
+      type: String,
+      default: 'primary',
+      validator (val) {
+        return ['primary'].includes(val)
+      },
+    },
+
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    typeClass () {
+      return `button--${this.type}`
+    },
+
+    sizeClass () {
+      return `button--${this.size}`
+    },
+
+    iconClass () {
+      return `fa-${this.icon}`
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  @extend %barlow;
+
+  border: none;
+  border-radius: $radius-4;
+  cursor: pointer;
+  font-size: 1rem;
+
+  @include transition;
+
+  &__icon {
+    @include margin(right, 1);
+  }
+
+  &:active {
+    @include scale-90;
+  }
+
+  &--normal {
+    padding: $base * 2 $base * 3;
+  }
+
+  &--small {
+    padding: $base * 2 $base * 2;
+  }
+
+  &--primary {
+    background: var(--color-primary);
+    color: white;
+
+    &:hover {
+      background: var(--color-primary-hover);
+    }
+  }
+}
+</style>
---
+++ tests/unit/specs/components/sad/SadButton.spec.js
@@ -0,0 +1,59 @@
+import SadButton from '@/components/sad/SadButton'
+import { factoryBuilder } from '#/factory-builder'
+
+const factory = (args = {}) => factoryBuilder(SadButton, {
+  propsData: {
+    type: 'primary',
+    size: 'normal',
+    ...args.propsData,
+  },
+})
+
+describe('SadButton', () => {
+  context('default behaviour', () => {
+    it('has primary type', () => {
+      const wrapper = factory()
+
+      expect(wrapper.vm.type).toEqual('primary')
+    })
+
+    it('has normal size', () => {
+      const wrapper = factory()
+
+      expect(wrapper.vm.size).toEqual('normal')
+    })
+  })
+
+  it('adds a class according to type', () => {
+    const wrapper = factory({ propsData: { type: 'primary' } })
+    const button = wrapper.find("[data-test='button']")
+
+    expect(button.classes()).toContain('button--primary')
+  })
+
+  it('adds a class according to size', () => {
+    const wrapper = factory({ propsData: { size: 'small' } })
+    const button = wrapper.find("[data-test='button']")
+
+    expect(button.classes()).toContain('button--small')
+  })
+
+  context('when icon prop is given', () => {
+    it('renders a matching icon', () => {
+      const wrapper = factory({ propsData: { icon: 'plus' } })
+      const icon = wrapper.find("[data-test='icon']")
+
+      expect(icon.classes()).toContain('fa-plus')
+    })
+  })
+
+  context('when button is clicked', () => {
+    it('emits click', async () => {
+      const wrapper = factory()
+
+      await wrapper.find("[data-test='button']").trigger('click')
+
+      expect(wrapper.emitted().click).toBeTruthy()
+    })
+  })
+})

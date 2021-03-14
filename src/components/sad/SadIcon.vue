c31db5141d9c3000c13433614b4f17423d210221
Ygor Sad <ysads2010@gmail.com>
2021-03-14 00:23:27 -0300

    feat(SadIcon): extract icon into component

---
+++ src/components/sad/SadIcon.vue
@@ -0,0 +1,78 @@
+<template>
+  <button
+    v-if="clickable"
+    class="sad-icon__button"
+    data-test="button"
+    @click="$emit('click')"
+  >
+    <i class="sad-icon icon fas" :class="classes" />
+  </button>
+  <i
+    v-else
+    class="sad-icon icon fas"
+    :class="classes"
+  />
+</template>
+
+<script>
+import { computed, defineComponent } from '@vue/composition-api'
+
+const COLORS = ['info', 'green', 'primary', 'red', 'yellow']
+const SIZES = ['small', 'medium']
+
+export default defineComponent({
+  name: 'SadIcon',
+  props: {
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
+    color: {
+      type: String,
+      default: 'gray',
+      validator: (val) => COLORS.includes(val),
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: 'small',
+      validator: (val) => SIZES.includes(val),
+    },
+  },
+  setup (props) {
+    const classes = computed(() => ([
+      `fa-${props.name}`,
+      `sad-icon--${props.color}`,
+      `sad-icon--${props.size}`,
+    ]))
+
+    return { classes }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.sad-icon {
+  &__button {
+    background: none;
+    cursor: pointer;
+    outline: none;
+
+    &:focus {
+      @include scale-90;
+    }
+  }
+
+  &--small { font-size: 16px; }
+  &--medium { font-size: 24px; }
+
+  &--green { color: var(--color-success);  }
+  &--info { color: var(--color-info); }
+  &--primary { color: var(--color-primary); }
+  &--red { color: var(--color-error); }
+  &--yellow { color: var(--color-warning); }
+}
+</style>
---
+++ tests/unit/specs/components/sad/SadIcon.spec.js
@@ -0,0 +1,53 @@
+import { factoryBuilder } from '#/factory-builder'
+import SadIcon from '@/components/sad/SadIcon'
+import sample from 'lodash/sample'
+
+const name = 'times'
+const size = sample(['small', 'medium'])
+const color = sample(['green', 'info', 'primary', 'red', 'yellow'])
+
+const factory = (args = {}) => factoryBuilder(SadIcon, {
+  propsData: {
+    name,
+    size,
+    color,
+    ...args,
+  },
+})
+
+describe('SadIcon', () => {
+  it('renders icon using given name', () => {
+    const wrapper = factory()
+
+    expect(wrapper.classes()).toContain(`fa-${name}`)
+  })
+
+  it('renders icon with size class', () => {
+    const wrapper = factory()
+
+    expect(wrapper.classes()).toContain(`sad-icon--${size}`)
+  })
+
+  it('renders icon with color class', () => {
+    const wrapper = factory()
+
+    expect(wrapper.classes()).toContain(`sad-icon--${color}`)
+  })
+
+  it('does not render a button wrapper by default', () => {
+    const wrapper = factory()
+    const item = wrapper.find("[data-test='button']")
+
+    expect(item.exists()).toBeFalsy()
+  })
+
+  describe('when clickable prop is true', () => {
+    it('emits click when button is clicked', () => {
+      const wrapper = factory({ clickable: true })
+
+      wrapper.find('button').trigger('click')
+
+      expect(wrapper.emitted().click).toBeTruthy()
+    })
+  })
+})

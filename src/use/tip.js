b6a00f13e033b5527d5b6835bb2b0f510a3ec556
Ygor Sad <ysads2010@gmail.com>
2021-04-09 13:23:54 -0300

    refactor(tip): add errorClass as computed

--- src/use/tip.js
+++ src/use/tip.js
@@ -3,8 +3,10 @@ import { computed } from '@vue/composition-api'
 export default function useTip (props) {
   const tipText = computed(() => props.error || props.tip)
   const tipVariant = computed(() => props.error ? 'error' : 'info')
+  const errorClass = computed(() => props.error ? 'error' : '')
 
   return {
+    errorClass,
     tipText,
     tipVariant,
   }
